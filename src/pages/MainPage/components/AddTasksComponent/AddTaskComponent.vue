915a29134507f16bea2010b9aa6c2d9815e6fe04
admin <tany--96@mail.ru>
2024-07-05 12:03:18 +0300

    состояние, структура, компоненты и пропсы

---
+++ src/pages/MainPage/components/AddTasksComponent/AddTaskComponent.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const { onAddTask } = defineProps<{
+  onAddTask: (taskTitle: string) => void
+}>()
+
+const inputTitle = ref('')
+
+const handleInputChange = (e: Event) => {
+  const target = e.target as HTMLInputElement
+  inputTitle.value = target.value
+}
+
+const addTaskHandler = () => {
+  if (!inputTitle.value.trim()) {
+    return
+  }
+  onAddTask(inputTitle.value)
+  inputTitle.value = ''
+}
+</script>
+
+<template>
+  <input
+    type="text"
+    @change="handleInputChange"
+    :value="inputTitle"
+    placeholder="Название задачи"
+  />
+  <button @click="addTaskHandler">Создать задачу</button>
+</template>
+
+<style scoped></style>
